7a2bdb57f61bea38cfc24c09404a95b93a84d222
lcs-ashin <ashin@lcs.on.ca>
2021-12-10 08:44:20 -0500

    Initial Commit

---
+++ MenuDecider/MenuDeciderApp.swift
@@ -0,0 +1,17 @@
+//
+//  MenuDeciderApp.swift
+//  MenuDecider
+//
+//  Created by Eunbi Shin on 2021-12-10.
+//
+
+import SwiftUI
+
+@main
+struct MenuDeciderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
