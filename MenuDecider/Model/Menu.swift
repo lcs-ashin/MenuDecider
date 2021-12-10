02f845a9999626cd2893f5c96b6b790c7c6700b9
lcs-ashin <ashin@lcs.on.ca>
2021-12-10 09:13:03 -0500

    Made a model page for the list of recommendations.

---
+++ MenuDecider/Model/Menu.swift
@@ -0,0 +1,45 @@
+//
+//  Menu.swift
+//  MenuDecider
+//
+//  Created by Eunbi Shin on 2021-12-10.
+//
+
+import Foundation
+
+struct Menu {
+    let recommendation: String
+}
+
+let listOfBreakfast = [
+
+    Menu(recommendation: "Omelette")
+    ,
+    Menu(recommendation: "Cereal")
+    ,
+    Menu(recommendation: "Toast")
+    ,
+    Menu(recommendation: "Yogurt")
+]
+
+let listOfLunch = [
+
+    Menu(recommendation: "Burger and fries")
+    ,
+    Menu(recommendation: "Fish and Chips")
+    ,
+    Menu(recommendation: "Sandwich")
+    ,
+    Menu(recommendation: "Ramen")
+]
+
+let listOfDinner = [
+
+    Menu(recommendation: "Sushi")
+    ,
+    Menu(recommendation: "Steak")
+    ,
+    Menu(recommendation: "Ribs")
+    ,
+    Menu(recommendation: "Pasta")
+]
